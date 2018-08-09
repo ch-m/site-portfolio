32f23afeb09b871c5f5e788d0798d698e525dc8a
unknown <christianufg@outlook.com>
2018-08-09 08:52:55 -0300

    overlay finalizado

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,8 +3,10 @@ const menu = document.querySelector(".menu");
 const menuBrading = document.querySelector(".menu-branding");
 const menuNav = document.querySelector(".menu-nav");
 const navItens = document.querySelectorAll(".nav-item");
+const divFoto = document.querySelector(".foto");
 
 let mostrarMenu = false;
+let tempoAnimacao = 5000;
 
 menuBtn.addEventListener("click", toggleMenu);
 
@@ -14,20 +16,29 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBrading.classList.add("show");
+    divFoto.classList.add("show");
     navItens.forEach(item => {
       item.classList.add("show");
     });
-
+    setTimeout(() => {
+      menuNav.style.transitionDelay = "0.5s";
+      menuBrading.style.transitionDelay = "0.5s";
+    }, 500);
     mostrarMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
+    divFoto.classList.remove("show");
     menuNav.classList.remove("show");
     menuBrading.classList.remove("show");
     navItens.forEach(item => {
       item.classList.remove("show");
     });
 
+    setTimeout(() => {
+      menuNav.style.transitionDelay = "0s";
+      menuBrading.style.transitionDelay = "0s";
+    }, 500);
     mostrarMenu = false;
   }
 }
